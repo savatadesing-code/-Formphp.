70b6c052c970ffcaf7e84ba56ff9af81315cb31b
savatadesing-code <savatadesing@gmail.com>
2025-09-13 08:49:27 -0500

    Update informacion.php

--- informacion.php
+++ informacion.php
@@ -1,1 +1,12 @@
+<?php
+
+$nombre = $_POST['nombre'];
+$edad = $_POST['edad'];
+$direccion = $_POST['direccion'];
+$salario = $_POST['salario'];
+
+echo "<h2>Información recibida</h2>";
+echo "El empleado $nombre tiene $edad años.<br>";
+echo "Vive en $direccion y su salario es: $salario.";
+?>
 
